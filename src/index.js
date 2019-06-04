a5c95c28156efeb7ed160180abbd814f5d5be85a
cruel-32 <cruel32@nate.com>
2019-06-04 19:12:42 +0900

    react + mobx 셋팅

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'mobx-react'; // MobX 에서 사용하는 Provider
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import RootStore from './stores';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = new RootStore(); // *** 루트 스토어 생성
+ReactDOM.render(
+  <Provider {...root}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
---
+++ src/pages/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() {
+        return (
+            <div className="flex_div">
+                <h1>React와 mobX를 활용한 상태관리와 React Router V4.</h1>
+            </div>
+        );
+    }
+}
+
+export default Counter;
